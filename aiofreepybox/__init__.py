ad474c293bc1718415bf47bf29a47da58f5acafc
stilllman <luc_touraille@yahoo.fr>
2018-12-04 23:12:50 +0100

    Bump version number

--- aiofreepybox/__init__.py
+++ aiofreepybox/__init__.py
@@ -5,7 +5,7 @@ Provides authentification and row access to Freebox using Freebox OS developer A
 Freebox API documentation : http://dev.freebox.fr/sdk/os/
 '''
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __all__ = ['aiofreepybox']
 
 from aiofreepybox.aiofreepybox import Freepybox
