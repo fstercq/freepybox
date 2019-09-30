e892b2e3d3cbe0c16e41cfc6b5afb3a24d2ba5d7
foreign-sub <51928805+foreign-sub@users.noreply.github.com>
2019-09-30 22:54:53 +0200

    Update connection.py (#14)
    Add get_lte_config
    Add set_lte_config

--- aiofreepybox/api/connection.py
+++ aiofreepybox/api/connection.py
@@ -3,32 +3,60 @@ class Connection:
     def __init__(self, access):
         self._access = access
 
-    async def get_status(self):
-        '''
-        Get connection status:
-        '''
-        return await self._access.get('connection/')
+    lte_configuration_data_schema = {
+        'enabled': True
+    }
 
     async def get_config(self):
         '''
-        Get connection configuration:
+        Get connection configuration
         '''
         return await self._access.get('connection/config/')
 
-    async def set_config(self, conf):
+    async def get_connection_logs(self):
         '''
-        Update connection configuration:
+        Get connection logs
         '''
-        await self._access.put('connection/config/', conf)
-
-    async def get_xdsl(self):
-        '''
-        Get xdsl infos:
-        '''
-        return await self._access.get('connection/xdsl/')
+        return await self._access.get('connection/logs/')
 
     async def get_ftth(self):
         '''
-        Get ftth infos:
+        Get ftth infos
         '''
         return await self._access.get('connection/ftth/')
+
+    async def get_lte_config(self):
+        '''
+        Get lte connection configuration
+        '''
+        return await self._access.get('connection/lte/config/')
+
+    async def get_status(self):
+        '''
+        Get connection status
+        '''
+        return await self._access.get('connection/')
+
+    async def get_xdsl(self):
+        '''
+        Get xdsl infos
+        '''
+        return await self._access.get('connection/xdsl/')
+
+    async def remove_connection_logs(self):
+        '''
+        Remove connection logs
+        '''
+        return await self._access.delete('connection/logs/')
+
+    async def set_config(self, connection_configuration):
+        '''
+        Update connection configuration
+        '''
+        await self._access.put('connection/config/', connection_configuration)
+
+    async def set_lte_config(self, lte_configuration_data=lte_configuration_data_schema):
+        '''
+        Update lte connection configuration
+        '''
+        await self._access.put('connection/lte/config/', lte_configuration_data)
