3c49ed89c56e76e790fac58d77b131def790ddeb
foreign-sub <51928805+foreign-sub@users.noreply.github.com>
2019-09-30 23:39:16 +0200

    Add support for the upnpav API (#36)

--- aiofreepybox/aiofreepybox.py
+++ aiofreepybox/aiofreepybox.py
@@ -24,6 +24,7 @@ from aiofreepybox.api.connection import Connection
 from aiofreepybox.api.nat import Nat
 from aiofreepybox.api.notifications import Notifications
 from aiofreepybox.api.rrd import Rrd
+from aiofreepybox.api.upnpav import Upnpav
 
 
 # Token file default location
@@ -79,6 +80,7 @@ class Freepybox:
         self.nat = Nat(self._access)
         self.notifications = Notifications(self._access)
         self.rrd = Rrd(self._access)
+        self.upnpav = Upnpav(self._access)
 
     async def close(self):
         '''
