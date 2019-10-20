f1d99ef7838b26f5c541fe5b784b95c6d0243dcb
foreign-sub <c_foreigner@yahoo.com>
2019-10-20 15:53:45 +0200

    Update airmedia.py: docstring, format

--- aiofreepybox/api/airmedia.py
+++ aiofreepybox/api/airmedia.py
@@ -1,41 +1,36 @@
 class Airmedia:
+    """
+    Airmedia
+    """
 
     def __init__(self, access):
         self._access = access
 
-    action = [
-        'start',
-        'stop'
-    ]
+    action = ["start", "stop"]
 
-    media_type = [
-        'photo',
-        'video'
-    ]
+    media_type = ["photo", "video"]
 
     airmedia_data_schema = {
-        'action': action[0],
-        'media': '',
-        'media_type': media_type[1],
-        'password': '',
-        'position': 0
+        "action": action[0],
+        "media": "",
+        "media_type": media_type[1],
+        "password": "",
+        "position": 0,
     }
 
-    airmedia_configuration_schema = {
-        'enabled': True
-    }
+    airmedia_configuration_schema = {"enabled": True}
 
     async def get_airmedia_configuration(self):
         """
         Get airmedia configuration
         """
-        return await self._access.get('airmedia/config/')
+        return await self._access.get("airmedia/config/")
 
     async def get_airmedia_receivers(self):
         """
         Get airmedia receivers
         """
-        return await self._access.get('airmedia/receivers/')
+        return await self._access.get("airmedia/receivers/")
 
     async def send_airmedia(self, receiver_name, airmedia_data):
         """
@@ -44,7 +39,7 @@ class Airmedia:
         receiver_name : `str`
         airmedia_data : `dict`
         """
-        await self._access.post('airmedia/receivers/{receiver_name}/', airmedia_data)
+        await self._access.post("airmedia/receivers/{receiver_name}/", airmedia_data)
 
     async def update_airmedia_configuration(self, airmedia_enabled):
         """
@@ -53,5 +48,5 @@ class Airmedia:
         airmedia_enabled : `bool`
         """
         airmedia_configuration_data = self.airmedia_configuration_schema
-        airmedia_configuration_data['enabled'] = airmedia_enabled
-        return await self._access.put('airmedia/config/', airmedia_configuration_data)
+        airmedia_configuration_data["enabled"] = airmedia_enabled
+        return await self._access.put("airmedia/config/", airmedia_configuration_data)
