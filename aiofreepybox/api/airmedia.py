7225854dca76e2208da128ffda41249c39776fba
foreign-sub <c_foreigner@yahoo.com>
2019-11-12 12:04:47 +0100

    Update airmedia.py: typing, fixes

--- aiofreepybox/api/airmedia.py
+++ aiofreepybox/api/airmedia.py
@@ -1,9 +1,13 @@
+from aiofreepybox.access import Access
+from typing import Any, Dict, List, Mapping, Optional
+
+
 class Airmedia:
     """
     Airmedia
     """
 
-    def __init__(self, access):
+    def __init__(self, access: Access) -> None:
         self._access = access
 
     action = ["start", "stop"]
@@ -18,28 +22,32 @@ class Airmedia:
         "position": 0,
     }
 
-    async def get_airmedia_configuration(self):
+    async def get_airmedia_configuration(self) -> Dict[str, bool]:
         """
         Get airmedia configuration
         """
         return await self._access.get("airmedia/config/")
 
-    async def get_airmedia_receivers(self):
+    async def get_airmedia_receivers(self) -> List[Dict[str, Any]]:
         """
         Get airmedia receivers
         """
         return await self._access.get("airmedia/receivers/")
 
-    async def send_airmedia(self, receiver_name, airmedia_data):
+    async def send_airmedia(
+        self, receiver_name: str, airmedia_data: Dict[str, Any]
+    ) -> None:
         """
         Send airmedia
 
         receiver_name : `str`
         airmedia_data : `dict`
         """
-        await self._access.post("airmedia/receivers/{receiver_name}/", airmedia_data)
+        await self._access.post(f"airmedia/receivers/{receiver_name}/", airmedia_data)
 
-    async def set_airmedia_configuration(self, airmedia_configuration_data):
+    async def set_airmedia_configuration(
+        self, airmedia_configuration_data: Dict[str, Any]
+    ) -> Dict[str, Any]:
         """
         Set airmedia configuration
 
@@ -48,33 +56,41 @@ class Airmedia:
         return await self._access.put("airmedia/config/", airmedia_configuration_data)
 
     async def update_airmedia_configuration(
-        self, airmedia_enabled=None, airmedia_password=None
-    ):
+        self,
+        airmedia_enabled: Optional[bool] = None,
+        airmedia_password: Optional[str] = None,
+    ) -> Optional[Dict[str, Any]]:
         """
         Update airmedia configuration
 
-        airmedia_enabled : `bool`
-        airmedia_password : `str`
+        airmedia_enabled : `bool`, optional
+            , Default to None
+        airmedia_password : `str`, optional
+            , Default to None
         """
 
         if airmedia_enabled is None and airmedia_password is None:
-            return
-        airmedia_configuration_data = {}
-        if airmedia_enabled is not None:
-            airmedia_configuration_data["enabled"] = airmedia_enabled
-        if airmedia_password is not None:
-            airmedia_configuration_data["password"] = airmedia_password
-        return await self.set_airmedia_configuration(airmedia_configuration_data)
+            return None
+        airmedia_c_enabled: Mapping[str, bool] = (
+            {"enabled": airmedia_enabled} if isinstance(airmedia_enabled, bool) else {}
+        )
+        airmedia_config: Dict[str, Any] = (
+            {**airmedia_c_enabled, "password": airmedia_password}
+            if isinstance(airmedia_password, str)
+            else {**airmedia_c_enabled}
+        )
 
-    async def airmedia_switch(self, enabled=None):
+        return await self.set_airmedia_configuration(airmedia_config)
+
+    async def airmedia_switch(self, enabled: Optional[bool] = None) -> bool:
         """
         Airmedia switch
 
-        airmedia_enabled : `bool`
+        airmedia_enabled : `bool`, optional
+            , Default to None
         """
 
         if enabled is None:
-            return await self.get_airmedia_configuration()["enabled"]
-        airmedia_configuration_data = {}
-        airmedia_configuration_data["enabled"] = enabled
-        return await self.set_airmedia_configuration(airmedia_configuration_data)
+            return (await self.get_airmedia_configuration())["enabled"]
+        airmedia_config = {"enabled": enabled}
+        return (await self.set_airmedia_configuration(airmedia_config))["enabled"]
